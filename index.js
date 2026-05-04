b3c75753f19d6d609ddaf5b1543a3fafc1c3a49c
Tony Brown <tony@Tonys-MacBook-Air.local>
2026-05-04 09:41:04 -0700

    fix code

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
-{
-  "name": "@daltonchristiano060-gif/supply",
-  "version": "1.0.0",
-  "description": "test",
-  "keywords": [
-    "supply"
-  ],
-  "homepage": "https://github.com/daltonchristiano060-gif/supply#readme",
-  "bugs": {
-    "url": "https://github.com/daltonchristiano060-gif/supply/issues"
-  },
-  "repository": {
-    "type": "git",
-    "url": "git+https://github.com/daltonchristiano060-gif/supply.git"
-  },
-  "license": "ISC",
-  "author": "dalton",
-  "type": "typescript",
-  "main": "index.js",
-  "scripts": {
-    "test": "echo \"ok\""
-  }
+
+function hello() {
+  return "Hello npm!";/Users/tony/work/supply/index.js
 }
+
+module.exports = hello;
+/Users/tony/work/supply/package.json
